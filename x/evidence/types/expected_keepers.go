0037f63948010b723edccabb484a7bf01e3f9c61
taeguk | thai <xornrbboy@gmail.com>
2025-04-21 16:43:54 +0900

    fix(x/evidence): validator should be slashed even if a validator is not bonded in active set (#2)

--- x/evidence/types/expected_keepers.go
+++ x/evidence/types/expected_keepers.go
@@ -17,7 +17,6 @@ type (
 	// ValidatorI expected validator interface
 	ValidatorI interface {
 		IsJailed() bool                          // whether the validator is jailed
-		IsUnbonded() bool                        // whether the validator is unbonded
 		ConsPubKey() (cryptotypes.PubKey, error) // validation consensus pubkey (cryptotypes.PubKey)
 		GetConsAddr() ([]byte, error)            // validation consensus address
 	}
